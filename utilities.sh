c1f14819deebc0e77a1ad631feca0b10c81868bb
Jonas <j.lewe@campus.lmu.de>
2020-06-05 13:02:26 +0200

    update utilities.sh

--- utilities.sh
+++ utilities.sh
@@ -1,11 +1,25 @@
-# install balena etcher
+#!/bin/bash
+
+# organize things
+mkdir /home/$USER/Apps
+
+# install utilities
+sudo apt update
+sudo apt install htop snapd wget git -y
+
+# install balena-etcher
 echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-sudo apt update && sudo apt install balena-etcher-electron
+sudo apt update
+sudo apt install balena-etcher-electron -y
 
-# install htop
+
 # install joplin
+wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
+
 # install keepassxc
+sudo snap install keepassxc
+
 # install vlc
 # install zoom
 # install flameshot
