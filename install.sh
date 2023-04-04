75590b89a90fde584f2b2585bc4796e4629c9088
Jonas Lewe <j.lewe@gmx.net>
2023-04-04 15:34:52 +0200

    update install

--- install.sh
+++ install.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
-rm -rf ~/.config/nvim
+if which nvim >/dev/null; then
+    echo "Neovim version $(nvim --version | head -1 | awk '{print $2}') is already installed."
+else
+    # Install Neovim
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
+    sudo mv nvim.appimage /usr/local/bin/nvim
+fi
 
-mkdir -p ~/.config/nvim
+config_dir=~/.config/nvim
 
-ln -s $(pwd)/nvim ~/.config/nvim
+if [[ -e $config_dir ]]; then
+    read -p "Neovim configuration already exists. Do you want to overwrite it? (y/n) " -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        rm -rf $config_dir
+        mkdir -p $config_dir
+        ln -s $(pwd)/nvim/init.vim $config_dir/init.vim
+    else
+        echo "Exiting neovim configuration setup."
+        exit 0
+    fi
+fi
+
+
