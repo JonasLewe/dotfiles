b0c68d8abde329ca6a6b4eec6ff161ace182dbd7
Jonas Lewe <j.lewe@gmx.net>
2023-04-04 16:43:23 +0200

    install packer

--- install.sh
+++ install.sh
@@ -29,4 +29,6 @@ fi
 
 mkdir -p $config_dir
 ln -s $(pwd)/nvim/ $config_dir/nvim 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 echo "Successfully installed neovim configuration."
