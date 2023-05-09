c881968db05bfbf9a0217a3f544ab838a1eebecc
Jonas Lewe <j.lewe@gmx.net>
2023-05-09 14:57:42 +0200

    update

---
+++ nvim/lua/jlewe/plugins/lsp/mason.lua
@@ -0,0 +1,35 @@
+-- import mason plugin safely
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+  return
+end
+
+-- import mason-lspconfig plugin safely
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+  return
+end
+
+-- enable mason
+mason.setup()
+
+mason_lspconfig.setup({
+  -- list of servers for mason to install
+  ensure_installed = {
+    -- "tsserver",
+    -- "html",
+    -- "cssls",
+	-- "jsonls",
+	"marksman", -- markdown lsp
+	"ltex", -- latex lsp
+    -- "tailwindcss",
+    -- "sumneko_lua",
+    "lua_ls",
+    -- "jedi_language_server", -- python lsp
+    -- "sqlls", 
+    -- "dockerls", 
+    -- "docker_compose_language_service", 
+  },
+  -- auto-install configured servers (with lspconfig)
+  -- automatic_installation = true, -- not the same as ensure_installed
+})
